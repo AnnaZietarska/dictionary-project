41d5b5c44ada4e5118647674202ff92c07153cde
AnnaZietarska <anna.zietarska@gmail.com>
2021-11-04 13:01:56 +0100

    Added dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        Hello
+        <footer className="text-center">Coded by Anna Ziętarska</footer>
+      </div>
     </div>
   );
 }
